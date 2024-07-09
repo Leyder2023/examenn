eb78ced2d2f7c39861295873ebb2cd3ebd1fd8a9
Leyder2023 <151747360+Leyder2023@users.noreply.github.com>
2024-07-09 11:58:41 -0500

    Add files via upload

---
+++ examenRecu/src/main/java/com/mycompany/examenrecu/ExamenRecu.java
@@ -0,0 +1,18 @@
+
+package com.mycompany.examenrecu;
+
+
+public class ExamenRecu {
+
+    public static void main(String[] args) {
+        double nombreEmpleado;
+       double  sueldoActual;
+       double sueldoNuevo;
+       double valorAumentado; 
+       
+       double sueldoActual = $500.000;
+       double aumentoUno = 8%
+       double aumentoDos = 10%        
+       
+    }
+}
